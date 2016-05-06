ecfcd0bb9d504a6c33b225018eaae777ff8cee20
sergio <sergio.jara@airbits.com.ar>
2016-05-06 02:45:01 -0300

    BE first commit

---
+++ backend/EscuelasWebApi/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using EscuelasWebApi;
+
+[assembly: OwinStartup(typeof(EscuelasWebApi.Startup))]
+namespace EscuelasWebApi { 
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+
+    }
+}
